8d680375103d0d8c821c4edf2ab89ffd18c09a02
Stephen <138625055+stephendotgg@users.noreply.github.com>
2024-09-20 20:46:24 +0100

    Convert layout to new structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,14 +6,37 @@
     import "../app.css";
 </script>
 
-<div class="absolute px-7 h-[75px] flex items-center justify-between">
-    <a href="/" class="flex items-center gap-3">
-        <img src="logo.png" class="w-8 h-8">
-        <p class="text-2xl font-[475]">Proposal Builder</p>
-    </a>
-</div>
-<div class="w-screen h-screen custom-gradient-bg">
-    <slot />
+<div class="w-screen h-screen flex flex-col custom-gradient-bg">
+    <div class="px-7 h-[75px] flex items-center justify-between">
+        <a href="/" class="flex items-center gap-3">
+            <img src="logo.png" class="w-8 h-8">
+            <p class="text-2xl font-[475]">Proposal Builder</p>
+        </a>
+    </div>
+    <div class="flex h-full">
+        <div class="h-full pl-7 flex flex-col pt-[1%] gap-10">
+            <a href="/new" class="ml-auto border-2 border-green-500 rounded-full py-1 px-6 text-green-600 text-lg whitespace-nowrap">+ Create new</a>
+            <div class="flex flex-col gap-4 text-xl font-semibold">
+                <a href="/drafts" class="flex gap-4 items-center">
+                    <img src="/icon/paperclip.svg" class="w-6 h-6 mt-0.5">
+                    <p>Drafts</p>
+                </a>
+                <a href="/proposals" class="flex gap-4 items-center">
+                    <img src="/icon/book.svg" class="w-6 h-6 mt-0.5">
+                    <p>Proposals</p>
+                </a>
+                <a href="/personas" class="flex gap-4 items-center">
+                    <img src="/icon/person.svg" class="w-6 h-6 mt-0.5">
+                    <p>Personas</p>
+                </a>
+                <a href="/settings" class="flex gap-4 items-center">
+                    <img src="/icon/cog.svg" class="w-6 h-6 mt-0.5">
+                    <p>Settings</p>
+                </a>
+            </div>
+        </div>
+        <slot />
+    </div>
 </div>
 
 <style>
